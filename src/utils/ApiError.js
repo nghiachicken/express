a6362d6f31340db3e9c5b49b563422a9538dfead
Nghia Nguyen <nghiachicken0511@gmail.com>
2021-12-10 17:02:02 +0800

    first commit

---
+++ src/utils/ApiError.js
@@ -0,0 +1,15 @@
+class ApiError extends Error {
+  constructor(statusCode, message, errors = [], isOperational = true, stack = '') {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    this.errors = errors;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+module.exports = ApiError;
